42039a85d039367127164f8e01c0d194cc1b6453
RyannWongg <35087429+RyannWongg@users.noreply.github.com>
2025-10-19 17:06:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -169,6 +169,7 @@ function renderDonut() {
      .attr('text-anchor','middle')
      .attr('dy','0.35em')
      .attr('font-size', '22')
+     .attr('color', 'white')
      .text(d => (d === '–' ? '–' : d + '%'));
 }
 
